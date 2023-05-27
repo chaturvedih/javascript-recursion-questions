0bbd2d0d1f2405caf54e3e1a28b5781824accd0a
Himanshu Chaturvedi <himanshu.chaturvedi@pristyncare.com>
2023-05-27 13:17:19 +0530

    question8 with rec + loop

--- question8.js
+++ question8.js
@@ -1,21 +1,38 @@
 // Question 8: Write a function that sums squares of numbers in list that may contain more lists
 
+// solution through recursion + for loop
 function SumSquares(arr){
-  if(arr.length===0) return 0
-  
+  if(arr.length === 0) return 0
   let count = 0
-  const num = arr.shift()
 
-  if(typeof num === 'number') {
-    count = count + num**2
+  for(let i=0; i< arr.length; i++){
+    if(Array.isArray(arr[i])){
+      count = count + SumSquares(arr[i])
+    }
+    else if(typeof arr[i] === 'number'){
+      count += arr[i]**2 
+    }
   }
-  else if(Array.isArray(num)){
-    count= count + SumSquares(num)
-  }
-  
-  return count + SumSquares(arr)
-  
+  return count
 }
+
+// solution through recursion
+// function SumSquares(arr){
+//   if(arr.length===0) return 0
+  
+//   let count = 0
+//   const num = arr.shift()
+
+//   if(typeof num === 'number') {
+//     count = count + num**2
+//   }
+//   else if(Array.isArray(num)){
+//     count= count + SumSquares(num)
+//   }
+  
+//   return count + SumSquares(arr)
+  
+// }
 
 let arr = [1,2,3]; 
 console.log(SumSquares(arr)); // 1 + 4 + 9 = 14
